003b8d28ea5080a3ecad025d282941e285008747
JeremyPattman <jeremypattman@jeremys-mbp.home>
2018-11-03 16:43:35 +0000

    basic Jinja2 loop structure to draw the snake board

--- Snake/Portal/app/app.py
+++ Snake/Portal/app/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, request, render_template
 
+squares=[
+    {
+	'fillStyle': '#01786F',
+	'x': 100,
+	'y': 100,
+	'len': 10
+	}
+]
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
-        return render_template('index.html')
+        return render_template('index.html', squares=squares)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
