546e408670ecdd164a5b4fe5dcf23a3933db771a
JeremyPattman <jeremypattman@jeremys-mbp.home>
2018-11-03 16:55:55 +0000

    test for board building

--- Snake/Portal/app/app.py
+++ Snake/Portal/app/app.py
@@ -1,19 +1,27 @@
 from flask import Flask, request, render_template
 
-squares=[
-    {
-	'fillStyle': '#01786F',
-	'x': 100,
-	'y': 100,
-	'len': 10
-	}
-]
+
+
 
 app = Flask(__name__)
 
+def addSquare(squares, x,y,type):
+    sq = {}
+    sq.update( {'x' : x} )
+    sq.update( {'y' : y} )
+    sq.update( {'len' : 10} )
+    sq.update( {'fillStyle': '#01786F'} )
+    squares.append(sq)
+
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
-        return render_template('index.html', squares=squares)
+
+    squares=[]
+    addSquare(squares,0,0,1)
+    addSquare(squares,10,0,1)
+    addSquare(squares,30,0,1)
+
+    return render_template('index.html', squares=squares)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
