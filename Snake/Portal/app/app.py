6e3632146af6d5d0a541a0cd3d99d4517a56637b
JeremyPattman <jeremypattman@jeremys-mbp.home>
2018-11-04 11:50:11 +0000

    more work on the snake board

--- Snake/Portal/app/app.py
+++ Snake/Portal/app/app.py
@@ -11,21 +11,28 @@ def addSquare(squares,x,y,len,type):
     sq.update( {'x' : x*len} )
     sq.update( {'y' : y*len} )
     sq.update( {'len' : len} )
-    sq.update( {'fillStyle': '#01786F'} )
+    if type == 1:
+        sq.update( {'fillStyle': '#BF4F51'} )
+    else:
+        sq.update( {'fillStyle': '#01786F'} )
+
     squares.append(sq)
 
 def createPlayArea(squares):
     for y in range(PLAY_AREA_HEIGHT):
         for x in range(PLAY_AREA_WIDTH):
-            addSquare(squares,x,y,BLOCK_SIZE,1)
+            if x==0 or x==(PLAY_AREA_WIDTH-1) or y==0 or y==(PLAY_AREA_HEIGHT-1):
+                addSquare(squares,x,y,BLOCK_SIZE,1)
+            else:
+                addSquare(squares,x,y,BLOCK_SIZE,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
-    squares=[]
-    createPlayArea(squares)
+    displaySquares=[]
+    createPlayArea(displaySquares)
 
-    return render_template('index.html', squares=squares)
+    return render_template('index.html', squares=displaySquares)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
